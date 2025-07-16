e101f1a5db5d6c8770ee2689ae24b87aadcf6fee
zyq <zhangyq@bit.edu.cn>
2025-07-16 14:47:38 +0800

    add scikit

---
+++ scikit-learn-extra/conftest.py
@@ -0,0 +1,38 @@
+import sys
+from packaging.version import Version
+import sklearn
+
+import pytest
+from _pytest.doctest import DoctestItem
+
+
+def pytest_collection_modifyitems(config, items):
+    # numpy changed the str/repr formatting of numpy arrays in 1.14. We want to
+    # run doctests only for numpy >= 1.14.
+    skip_doctests = False
+    try:
+        import numpy as np
+
+        if Version(np.__version__) < Version("1.14") or Version(
+            sklearn.__version__
+        ) < Version("0.23.0"):
+            reason = (
+                "doctests are only run for numpy >= 1.14 "
+                "and scikit-learn >=0.23.0"
+            )
+            skip_doctests = True
+        elif sys.platform.startswith("win32"):
+            reason = (
+                "doctests are not run for Windows because numpy arrays "
+                "repr is inconsistent across platforms."
+            )
+            skip_doctests = True
+    except ImportError:
+        pass
+
+    if skip_doctests:
+        skip_marker = pytest.mark.skip(reason=reason)
+
+        for item in items:
+            if isinstance(item, DoctestItem):
+                item.add_marker(skip_marker)
---
+++ scikit-learn-extra/sklearn_extra/tests/test_common.py
@@ -0,0 +1,49 @@
+import pytest
+from sklearn.utils import estimator_checks
+
+from sklearn_extra.kernel_approximation import Fastfood
+from sklearn_extra.kernel_methods import EigenProClassifier, EigenProRegressor
+from sklearn_extra.cluster import KMedoids, CommonNNClustering, CLARA
+from sklearn_extra.robust import (
+    RobustWeightedClassifier,
+    RobustWeightedRegressor,
+    RobustWeightedKMeans,
+)
+
+
+ALL_ESTIMATORS = [
+    Fastfood,
+    KMedoids,
+    CLARA,
+    EigenProClassifier,
+    EigenProRegressor,
+    CommonNNClustering,
+    RobustWeightedKMeans,
+    RobustWeightedRegressor,
+    RobustWeightedClassifier,
+]
+
+
+@estimator_checks.parametrize_with_checks([cls() for cls in ALL_ESTIMATORS])
+def test_all_estimators(estimator, check, request):
+    # TODO: fix this common test failure cf #41
+    if isinstance(
+        estimator, EigenProClassifier
+    ) and "function check_classifier_multioutput" in str(check):
+        request.applymarker(
+            pytest.mark.xfail(run=False, reason="See issue #41")
+        )
+
+    # TODO: fix this later, ask people at sklearn to advise on it.
+    if isinstance(estimator, RobustWeightedRegressor) and (
+        ("function check_regressors_train" in str(check))
+        or ("function check_estimators_dtypes" in str(check))
+    ):
+        request.applymarker(pytest.mark.xfail(run=False))
+    if isinstance(estimator, RobustWeightedClassifier) and (
+        ("function check_classifiers_train" in str(check))
+        or ("function check_estimators_dtypes" in str(check))
+    ):
+        request.applymarker(pytest.mark.xfail(run=False))
+
+    return check(estimator)
